58aaad8170bf1c89d25b642e2ad966910664a0b8
Rodrigo Klosowski <klosowsk@gmail.com>
2026-03-18 13:05:59 +0000

    terminal keymap

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,3 +1,34 @@
 return {
-  {'akinsho/toggleterm.nvim', version = "*", config = true}
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    keys = {
+      { "<C-\\>", "<cmd>ToggleTerm<cr>", desc = "Toggle terminal", mode = {"n", "t", "i"} },
+      { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle floating terminal", mode = {"n", "t"} },
+    },
+    config = function()
+      require("toggleterm").setup({
+        open_mapping = [[<c-\>]],
+        terminal_mappings = true,
+        insert_mappings = true,
+        direction = "horizontal",
+        size = 15,
+        float_opts = {
+          border = "curved",
+          winblend = 3,
+        },
+      })
+      
+      -- Terminal-specific keymaps for navigation
+      function _G.set_terminal_keymaps()
+        local opts = {buffer = 0}
+        vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+        vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+        vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+        vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+      end
+
+      vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
+    end
+  }
 }
